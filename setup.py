580692e75f95555d3c31f46cfb68df2690526085
michael.ward <ward@Michaels-MacBook-Pro.local>
2021-01-25 17:49:35 -0700

    add data to setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+setup(name='wpm',
+      version='0.0.1',
+      description='Ward Package Manager',
+      author='Michael Ward',
+      author_email='na@na.com',
+      url='https://github.com/mward29/wpm.git',
+     )
