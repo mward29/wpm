c436d87d65302c23f9a552f2127d3d5c2eb65ba7
michael.ward <ward@Michaels-MacBook-Pro.local>
2021-01-25 17:37:15 -0700

    record multi-line

--- main.py
+++ main.py
@@ -8,8 +8,40 @@ LOG = logger.get_root_logger(
     __name__, filename=os.path.join(root_path, 'output.log'))
 
 
+# intro to wpm
+def intro():
+    print("""
+Welcome to the Ward Package Manager
+Where the force MAY be with you ...
+""")
+
+def parse_lines(lines, packages):
+    """
+    Parse lines and execute commands
+    """
+    for line in lines:
+        x = line.split(' ')
+        cmd = x[0].upper()
+        LOG.debug(cmd)
+
+
 def run():
+    """
+    Primary runtime of WPM
+    """
     LOG.debug("and so it begins")
+    intro()
+
+    lines = []
+    print("Awaiting your input: ")
+    test = ''
+    while test != 'END':
+        line = input()
+        if line != "END":
+            lines.append(line)
+        else:
+            test = 'END'
+    LOG.debug(lines)
 
 
 # execute if main
